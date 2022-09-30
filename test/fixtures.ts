bbd3eef85d530729652e4ba9d524cf73e643bef4
Espen Hovlandsdal <espen@hovlandsdal.com>
2022-09-30 15:10:18 -0700

    feat: initial commit

---
+++ test/fixtures.ts
@@ -0,0 +1,329 @@
+import {encodeData, formatComment, formatEvent} from './format'
+import {MULTIBYTE_EMOJIS, MULTIBYTE_LINES} from './multibyte'
+
+type OnChunkCallback = (chunk: string) => void
+
+const TEN_MEGABYTES = 1024 * 1024 * 10
+const EMOJI_DATA = MULTIBYTE_EMOJIS.join(' ')
+const DATA_CHUNK = encodeData(`${MULTIBYTE_LINES.join('\n\n')}\n${EMOJI_DATA}`).trim()
+const DATA_CHUNK_LENGTH = Buffer.from(DATA_CHUNK).byteLength
+const DATA_CHUNK_WAIT = Math.floor(1000 / (TEN_MEGABYTES / DATA_CHUNK_LENGTH))
+
+export async function getBasicFixtureStream(onChunk: OnChunkCallback): Promise<void> {
+  for (let i = 0; i < 5; i++) {
+    onChunk(formatEvent({data: `${i}`}))
+    await delay(250)
+  }
+
+  onChunk(formatEvent({event: 'done', data: '✔'}))
+}
+
+export async function getTimeFixtureStream(onChunk: OnChunkCallback): Promise<void> {
+  for (let i = 0; i < 5; i++) {
+    onChunk(formatEvent({event: 'time', data: new Date().toISOString()}))
+    await delay(250)
+  }
+
+  onChunk(formatEvent({event: 'done', data: '✔'}))
+}
+
+export async function getTimeFixtureStreamChunked(onChunk: OnChunkCallback): Promise<void> {
+  for (let i = 0; i < 30; i++) {
+    await enqueRandomChunks(
+      formatEvent({id: randomString(), event: 'time', data: new Date().toISOString()}),
+      onChunk
+    )
+  }
+}
+
+export async function getIdentifiedFixtureStream(
+  start: number,
+  onChunk: OnChunkCallback
+): Promise<void> {
+  for (let id = start; id < start + 2; id++) {
+    onChunk(formatEvent({event: 'tick', data: `${id}`, id: `${id}`, retry: 50}))
+    await delay(15)
+  }
+
+  if (start >= 4) {
+    onChunk(formatEvent({event: 'done', data: '✔'}))
+  }
+}
+
+export async function getHeartbeatsFixtureStream(onChunk: OnChunkCallback): Promise<void> {
+  for (let i = 0; i < 5; i++) {
+    onChunk(formatComment(' ♥'))
+    onChunk(formatEvent(String.fromCharCode(65 + i)))
+    await delay(15)
+  }
+
+  onChunk(formatEvent({event: 'done', data: '✔'}))
+}
+
+export async function getMultilineFixtureStream(onChunk: OnChunkCallback): Promise<void> {
+  onChunk('event: stock\n')
+  onChunk('data: YHOO\n')
+  onChunk('data: +2\n')
+  onChunk('data: 10\n\n')
+
+  await delay(250)
+
+  onChunk('event: stock\n')
+  onChunk('data: GOOG\n')
+  onChunk('data: -8\n')
+  onChunk('data: 1881\n\n')
+
+  onChunk(formatEvent({event: 'done', data: '✔'}))
+}
+
+export async function getMultibyteFixtureStream(onChunk: OnChunkCallback): Promise<void> {
+  const emojis = MULTIBYTE_EMOJIS
+  const lines = MULTIBYTE_LINES
+  const emojisPerMessage = Math.ceil(emojis.length / lines.length)
+
+  for (let i = 0; i < lines.length; i++) {
+    const split = i % 2 === 0 // Split into separate data chunks in the case of even lines
+    const linejis = emojis
+      .slice(emojisPerMessage * i, emojisPerMessage * i + emojisPerMessage)
+      .join(' ')
+    const line = `${lines[i]} ${linejis}`
+    onChunk(`id: ${i}\n`)
+    if (split) {
+      onChunk(`data:${line.slice(0, 5)}\n`)
+      onChunk(`data:${line.slice(5)}\n\n`)
+    } else {
+      onChunk(`data:${line}\n\n`)
+    }
+    await delay(50)
+  }
+
+  onChunk(formatEvent({event: 'done', data: '✔'}))
+}
+
+export async function getMultibyteEmptyLineFixtureStream(onChunk: OnChunkCallback): Promise<void> {
+  await delay(1)
+
+  onChunk('\n\n\n\nid: 1\ndata: 我現在都看實況不玩遊戲\n\n')
+  onChunk(formatEvent({event: 'done', data: '✔'}))
+}
+
+export async function getInvalidBomFixtureStream(onChunk: OnChunkCallback): Promise<void> {
+  await delay(1)
+
+  onChunk('\uFEFFdata: bomful 1\n\n')
+  onChunk('\uFEFFdata: bomful 2\n\n')
+  onChunk('data: bomless 3\n\n')
+  onChunk(formatEvent({event: 'done', data: '✔'}))
+}
+
+export async function getBomFixtureStream(onChunk: OnChunkCallback): Promise<void> {
+  await delay(1)
+
+  const bom = [239, 187, 191].map((ascii) => String.fromCharCode(ascii)).join('')
+
+  onChunk(`${bom}data: bomful 1\n\n`)
+  onChunk(`${bom}data: bomful 2\n\n`)
+  onChunk('data: bomless 3\n\n')
+  onChunk(formatEvent({event: 'done', data: '✔'}))
+}
+
+export async function getCarriageReturnFixtureStream(onChunk: OnChunkCallback): Promise<void> {
+  await delay(1)
+
+  onChunk('data: dog\r')
+  onChunk('data: bark\r\r')
+
+  onChunk('data: cat\r')
+  onChunk('data: meow\r\r')
+
+  onChunk(formatEvent({event: 'done', data: '✔'}))
+}
+
+export async function getLineFeedFixtureStream(onChunk: OnChunkCallback): Promise<void> {
+  await delay(1)
+
+  onChunk('data: cow\n')
+  onChunk('data: moo\n\n')
+
+  onChunk('data: horse\n')
+  onChunk('data: neigh\n\n')
+
+  onChunk(formatEvent({event: 'done', data: '✔'}))
+}
+
+export async function getCarriageReturnLineFeedFixtureStream(
+  onChunk: OnChunkCallback
+): Promise<void> {
+  await delay(1)
+
+  onChunk('data: sheep\r\n')
+  onChunk('data: bleat\r\n\r\n')
+
+  onChunk('data: pig\r\n')
+  onChunk('data: oink\r\n\r\n')
+
+  onChunk(formatEvent({event: 'done', data: '✔'}))
+}
+
+export async function getCommentsFixtureStream(onChunk: OnChunkCallback): Promise<void> {
+  onChunk(': Hello\n\n')
+  onChunk(':'.repeat(300))
+  onChunk('\n')
+  await delay(250)
+
+  onChunk('data: First\n\n')
+  onChunk(': Первый')
+  await delay(250)
+
+  onChunk(': 第二')
+  onChunk('\n')
+  onChunk('data: Second\n\n')
+  await delay(250)
+
+  for (let i = 0; i < 10; i++) {
+    onChunk(': Moop \n')
+  }
+
+  onChunk(': ثالث')
+  onChunk('\n')
+  onChunk('data: Third\n\n')
+  await delay(250)
+
+  onChunk(':നാലാമത്തെ')
+  onChunk('\n')
+  onChunk('data: Fourth\n\n')
+  await delay(250)
+
+  onChunk(`: ${MULTIBYTE_EMOJIS.slice(0, 100).join(' ')} :`)
+  onChunk('\n')
+  onChunk('data: Fifth\n\n')
+
+  onChunk(formatEvent({event: 'done', data: '✔'}))
+}
+
+export async function getMixedCommentsFixtureStream(onChunk: OnChunkCallback): Promise<void> {
+  const longString = 'x'.repeat(2 * 1024 + 1)
+  onChunk('data:1\r\r:\0\n:\r\ndata:2\n\n:')
+
+  await delay(50)
+
+  onChunk(longString)
+
+  await delay(50)
+
+  onChunk('\rdata:3\n\n:data:fail\r:')
+  onChunk(longString)
+
+  await delay(50)
+
+  onChunk('\ndata:4\n\n')
+  await delay(250)
+
+  onChunk('data:5')
+  await delay(250)
+}
+
+export async function getEmptyEventsFixtureStream(onChunk: OnChunkCallback): Promise<void> {
+  await delay(1)
+
+  onChunk('event:\ndata: Hello 1\n\n')
+  onChunk('event:\n\n')
+  onChunk(formatEvent({event: 'done', data: '✔'}))
+}
+
+export async function getEmptyRetryFixtureStream(
+  last: number | undefined,
+  onChunk: OnChunkCallback
+): Promise<void> {
+  await delay(1)
+
+  if (!last) {
+    onChunk(formatEvent({id: '1', retry: 500, data: '🥌'}))
+  } else if (last === 1) {
+    onChunk('id:2\nretry:\ndata:🧹\n\n')
+  } else {
+    onChunk(formatEvent({id: '3', data: '✅'}))
+  }
+}
+
+export async function getFieldParsingFixtureStream(onChunk: OnChunkCallback): Promise<void> {
+  await delay(1)
+
+  onChunk(
+    'data:\0\ndata:  2\rData:1\ndata\0:2\ndata:1\r\0data:4\nda-ta:3\rdata_5\ndata:3\rdata\ndata:\r\n data:32\ndata:4\n\n'
+  )
+  onChunk(formatEvent({event: 'done', data: '✔'}))
+}
+
+export async function getDataFieldParsingFixtureStream(onChunk: OnChunkCallback): Promise<void> {
+  await delay(1)
+
+  onChunk('data:\n\ndata\ndata\n\ndata:test\n\n')
+  onChunk(formatEvent({event: 'done', data: '✔'}))
+}
+
+export async function getInvalidRetryFixtureStream(onChunk: OnChunkCallback): Promise<void> {
+  await delay(1)
+
+  onChunk('retry:1000\nretry:2000x\ndata:x\n\n')
+}
+
+export async function getUnknownFieldsFixtureStream(onChunk: OnChunkCallback): Promise<void> {
+  await delay(1)
+
+  onChunk('data:abc\n data\ndata\nfoobar:xxx\njustsometext\n:thisisacommentyay\ndata:123\n\n')
+  onChunk(formatEvent({event: 'done', data: '✔'}))
+}
+
+export async function getHugeMessageFixtureStream(onChunk: OnChunkCallback): Promise<void> {
+  onChunk(': hello\n\n')
+
+  let written = 0
+  while (written < TEN_MEGABYTES) {
+    onChunk(DATA_CHUNK)
+    written += DATA_CHUNK_LENGTH
+    await delay(DATA_CHUNK_WAIT)
+  }
+
+  onChunk('\n\n')
+  onChunk(': END-OF-STREAM\n\n')
+  onChunk(
+    formatEvent({
+      event: 'done',
+      data: 'e094a44a2436226ea9feb04e413a28de012b406012ec0eb6b37ad0a19d403660',
+    })
+  )
+}
+
+export async function getHeadersFixtureStream(
+  headers: any,
+  onChunk: OnChunkCallback
+): Promise<void> {
+  await delay(1)
+
+  onChunk(formatEvent({event: 'headers', data: JSON.stringify(headers)}))
+}
+
+function delay(ms: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+function randomString() {
+  return `${Math.floor(Math.random() * 1e12)}`
+}
+
+function randomDelay(): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, Math.ceil(Math.random() * 30)))
+}
+
+function getRandomChunks(input: string): [string, string] {
+  const splitAt = Math.floor(Math.random() * Math.floor(input.length))
+  return [input.slice(0, splitAt), input.slice(splitAt)]
+}
+
+async function enqueRandomChunks(input: string, onChunk: OnChunkCallback) {
+  const [start, end] = getRandomChunks(input)
+  onChunk(start)
+  await randomDelay()
+  onChunk(end)
+}
---
+++ test/parse.test.ts
@@ -0,0 +1,326 @@
+import {createHash} from 'node:crypto'
+import {createParser} from '../src/parse'
+import type {ParsedEvent, ReconnectInterval} from '../src/types'
+import {
+  getBasicFixtureStream,
+  getBomFixtureStream,
+  getCarriageReturnFixtureStream,
+  getCarriageReturnLineFeedFixtureStream,
+  getCommentsFixtureStream,
+  getDataFieldParsingFixtureStream,
+  getEmptyEventsFixtureStream,
+  getEmptyRetryFixtureStream,
+  getHeartbeatsFixtureStream,
+  getHugeMessageFixtureStream,
+  getIdentifiedFixtureStream,
+  getInvalidBomFixtureStream,
+  getInvalidRetryFixtureStream,
+  getLineFeedFixtureStream,
+  getMixedCommentsFixtureStream,
+  getMultibyteEmptyLineFixtureStream,
+  getMultibyteFixtureStream,
+  getMultilineFixtureStream,
+  getTimeFixtureStream,
+  getTimeFixtureStreamChunked,
+  getUnknownFieldsFixtureStream,
+} from './fixtures'
+
+test('basic unnamed events stream', async () => {
+  const mock = getParseResultMock()
+  const parser = createParser(mock.onParse)
+  await getBasicFixtureStream(parser.feed)
+
+  mock.expectNumberOfMessagesToBe(6)
+  mock.expectNextMessage({data: '0', event: undefined, id: undefined})
+  mock.expectNextMessage({data: '1'})
+  mock.expectNextMessage({data: '2'})
+  mock.expectNextMessage({data: '3'})
+  mock.expectNextMessage({data: '4'})
+  mock.expectNextMessage({event: 'done', data: '✔'})
+})
+
+test('stream of `time` event name', async () => {
+  const mock = getParseResultMock()
+  const parser = createParser(mock.onParse)
+  await getTimeFixtureStream(parser.feed)
+
+  mock.expectNumberOfMessagesToBe(6)
+  mock.expectNextMessage({event: 'time', data: /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/})
+})
+
+test('stream of `time` event names, unbalanced chunks', async () => {
+  const mock = getParseResultMock()
+  const parser = createParser(mock.onParse)
+  await getTimeFixtureStreamChunked(parser.feed)
+
+  while (mock.hasNextMessage()) {
+    mock.expectNextMessage({event: 'time', data: /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/})
+  }
+})
+
+test('stream of identified messanges + retry interval', async () => {
+  const mock = getParseResultMock()
+  const parser = createParser(mock.onParse)
+  await getIdentifiedFixtureStream(1337, parser.feed)
+
+  expect(mock.events[0]).toMatchObject({type: 'reconnect-interval', value: 50})
+  expect(mock.events[1]).toMatchObject({type: 'event', id: '1337', event: 'tick', data: '1337'})
+  expect(mock.events[2]).toMatchObject({type: 'reconnect-interval', value: 50})
+  expect(mock.events[3]).toMatchObject({type: 'event', id: '1338', event: 'tick', data: '1338'})
+})
+
+test('stream of "heartbeat" comments, unnamed events', async () => {
+  const mock = getParseResultMock()
+  const parser = createParser(mock.onParse)
+  await getHeartbeatsFixtureStream(parser.feed)
+
+  for (let char = 65; char < 70; char++) {
+    mock.expectNextMessage({data: String.fromCharCode(char)})
+  }
+
+  mock.expectNextMessage({event: 'done', data: '✔'})
+})
+
+test('stream of multi-line data events', async () => {
+  const mock = getParseResultMock()
+  const parser = createParser(mock.onParse)
+  await getMultilineFixtureStream(parser.feed)
+  mock.expectNextMessage({
+    id: undefined,
+    event: 'stock',
+    data: 'YHOO\n+2\n10',
+  })
+  mock.expectNextMessage({
+    id: undefined,
+    event: 'stock',
+    data: 'GOOG\n-8\n1881',
+  })
+})
+
+test('stream of multi-byte events', async () => {
+  const mock = getParseResultMock()
+  const parser = createParser(mock.onParse)
+  await getMultibyteFixtureStream(parser.feed)
+  for (const event of mock.events) {
+    expect(event).toMatchSnapshot()
+  }
+})
+
+test('stream of multi-byte events with some empty lines thrown in', async () => {
+  const mock = getParseResultMock()
+  const parser = createParser(mock.onParse)
+  await getMultibyteEmptyLineFixtureStream(parser.feed)
+
+  mock.expectNextMessage({data: '我現在都看實況不玩遊戲'})
+  mock.expectNextMessage({event: 'done'})
+})
+
+test('stream containing (invalid?) byte-order mark (multiple places)', async () => {
+  const mock = getParseResultMock()
+  const parser = createParser(mock.onParse)
+  await getInvalidBomFixtureStream(parser.feed)
+  mock.expectNextMessage({data: 'bomless 3'})
+  mock.expectNextMessage({event: 'done'})
+})
+
+test('stream containing byte-order mark (multiple places)', async () => {
+  const mock = getParseResultMock()
+  const parser = createParser(mock.onParse)
+  await getBomFixtureStream(parser.feed)
+  mock.expectNextMessage({data: 'bomful 1'})
+  mock.expectNextMessage({data: 'bomless 3'})
+  mock.expectNextMessage({event: 'done'})
+})
+
+test('stream using carriage returns', async () => {
+  const mock = getParseResultMock()
+  const parser = createParser(mock.onParse)
+  await getCarriageReturnFixtureStream(parser.feed)
+  mock.expectNextMessage({data: 'dog\nbark'})
+  mock.expectNextMessage({data: 'cat\nmeow'})
+  mock.expectNextMessage({event: 'done'})
+})
+
+test('stream using line feeds', async () => {
+  const mock = getParseResultMock()
+  const parser = createParser(mock.onParse)
+  await getLineFeedFixtureStream(parser.feed)
+  mock.expectNextMessage({data: 'cow\nmoo'})
+  mock.expectNextMessage({data: 'horse\nneigh'})
+  mock.expectNextMessage({event: 'done'})
+})
+
+test('stream using carriage returns and line feeds', async () => {
+  const mock = getParseResultMock()
+  const parser = createParser(mock.onParse)
+  await getCarriageReturnLineFeedFixtureStream(parser.feed)
+  mock.expectNextMessage({data: 'sheep\nbleat'})
+  mock.expectNextMessage({data: 'pig\noink'})
+  mock.expectNextMessage({event: 'done'})
+})
+
+test('stream with varying odd uses of comments', async () => {
+  const mock = getParseResultMock()
+  const parser = createParser(mock.onParse)
+  await getCommentsFixtureStream(parser.feed)
+  mock.expectNextMessage({data: 'First'})
+  mock.expectNextMessage({data: 'Second'})
+  mock.expectNextMessage({data: 'Third'})
+  mock.expectNextMessage({data: 'Fourth'})
+  mock.expectNextMessage({data: 'Fifth'})
+  mock.expectNextMessage({data: '✔'})
+})
+
+test('stream with even more odd uses of comments', async () => {
+  const mock = getParseResultMock()
+  const parser = createParser(mock.onParse)
+  await getMixedCommentsFixtureStream(parser.feed)
+  mock.expectNextMessage({data: '1'})
+  mock.expectNextMessage({data: '2\n3\n4'})
+})
+
+test('stream with empty `event` field', async () => {
+  const mock = getParseResultMock()
+  const parser = createParser(mock.onParse)
+  await getEmptyEventsFixtureStream(parser.feed)
+  mock.expectNextMessage({data: 'Hello 1', event: undefined})
+  mock.expectNextMessage({data: '✔', event: 'done'})
+})
+
+test('stream with empty `retry` field', async () => {
+  const mock = getParseResultMock()
+  const parser = createParser(mock.onParse)
+  await getEmptyRetryFixtureStream(0, parser.feed)
+  await getEmptyRetryFixtureStream(1, parser.feed)
+  await getEmptyRetryFixtureStream(2, parser.feed)
+  expect(mock.events[0]).toMatchObject({
+    type: 'reconnect-interval',
+    value: 500,
+  })
+  expect(mock.events[1]).toMatchObject({
+    data: '🥌',
+    event: undefined,
+    id: '1',
+    type: 'event',
+  })
+  expect(mock.events[2]).toMatchObject({
+    data: '🧹',
+    event: undefined,
+    id: '2',
+    type: 'event',
+  })
+  expect(mock.events[3]).toMatchObject({
+    data: '✅',
+    event: undefined,
+    id: '3',
+    type: 'event',
+  })
+})
+
+test('stream with oddly shaped data field', async () => {
+  const mock = getParseResultMock()
+  const parser = createParser(mock.onParse)
+  await getDataFieldParsingFixtureStream(parser.feed)
+  mock.expectNextMessage({data: ''})
+  mock.expectNextMessage({data: '\n'})
+  mock.expectNextMessage({data: 'test'})
+})
+
+test('stream with partially incorrect retry fields', async () => {
+  const mock = getParseResultMock()
+  const parser = createParser(mock.onParse)
+  await getInvalidRetryFixtureStream(parser.feed)
+  expect(mock.events[0]).toMatchObject({type: 'reconnect-interval', value: 1000})
+  expect(mock.events[1]).toMatchObject({type: 'reconnect-interval', value: 2000})
+  expect(mock.events[2]).toMatchObject({type: 'event', data: 'x', event: undefined})
+  mock.expectNumberOfMessagesToBe(3)
+})
+
+test('stream with unknown fields in the stream', async () => {
+  const mock = getParseResultMock()
+  const parser = createParser(mock.onParse)
+  await getUnknownFieldsFixtureStream(parser.feed)
+  mock.expectNextMessage({event: undefined, data: 'abc\n\n123'})
+})
+
+test('stream with huge data chunks', async () => {
+  jest.setTimeout(15000)
+  const mock = getParseResultMock()
+  const parser = createParser(mock.onParse)
+  await getHugeMessageFixtureStream(parser.feed)
+  const hugeMsg = mock.events[0]
+  if (hugeMsg.type !== 'event') {
+    throw new Error('First message was not an event')
+  }
+
+  expect(hugeMsg.data.length).toBe(4808512)
+
+  const receivedHash = createHash('sha256').update(hugeMsg.data).digest('hex')
+  const hashMsg = mock.events[1]
+  if (hashMsg.type !== 'event') {
+    throw new Error('Second message was not an event')
+  }
+
+  expect(hashMsg.data).toBe(receivedHash)
+})
+
+interface MessageMatcher {
+  id?: string | RegExp
+  event?: string
+  data?: string | RegExp
+}
+
+function getParseResultMock() {
+  let messageIndex = -1
+  const events: (ParsedEvent | ReconnectInterval)[] = []
+  const fn = jest.fn((evt: ParsedEvent | ReconnectInterval) => events.push(evt))
+
+  function messageMatches(index: number, evt: MessageMatcher) {
+    const msg = events[index]
+    if (!msg) {
+      throw new Error(`No message found at index ${index}`)
+    }
+
+    if (msg.type !== 'event') {
+      throw new Error(`Expected parse result of type "event", got "${msg.type}"`)
+    }
+
+    if ('id' in evt) {
+      if (evt.id instanceof RegExp) {
+        expect(msg.id).toMatch(evt.id)
+      } else {
+        expect(msg.id).toBe(evt.id)
+      }
+    }
+
+    if ('event' in evt) {
+      expect(msg.event).toBe(evt.event)
+    }
+
+    if ('data' in evt) {
+      if (evt.data instanceof RegExp) {
+        expect(msg.data).toMatch(evt.data)
+      } else {
+        expect(msg.data).toBe(evt.data)
+      }
+    }
+  }
+
+  return {
+    onParse: fn,
+    events,
+    hasNextMessage() {
+      return events.length > messageIndex + 1
+    },
+    expectMessageAtIndexToMatch(index: 0, evt: MessageMatcher) {
+      return messageMatches(index, evt)
+    },
+    expectNextMessage(evt: MessageMatcher) {
+      messageIndex++
+      return messageMatches(messageIndex, evt)
+    },
+    expectNumberOfMessagesToBe(num: number) {
+      return expect(fn).toBeCalledTimes(num)
+    },
+  }
+}
